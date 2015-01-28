6276417c32067e6c875537d899b82544b8e1c4e6
antljones <antony.l.jones@gmail.com>
2015-01-28 15:14:13 +1300

    Added JSON and CSV format capabilities

--- geocoordinates/geocoordinate_retriever_and_formatter.rb
+++ geocoordinates/geocoordinate_retriever_and_formatter.rb
@@ -19,102 +19,181 @@ def retrieve_geocoordinate_resources(site)
     if link.key?("href")
       if link["href"].length > 7 and link["href"][0..7] == "cntyfile"
         if link["href"][0] != "#"
+          link_string = link["href"].to_s
+          link_string = link_string[9..-1]
+          `rm #{link_string}`
           `wget #{base_uri + link["href"]}`
-          `unzip *.zip`
-          `rm *.zip`
+          `mkdir #{link_string[0..link_string.index(".") - 1]}`
+          `unzip #{link_string} #{link_string[0..link_string.index(".") - 1]}`
+          `rm #{link_string}`
         end
       end
     end
   end
 end
 
+def retrieve_country_code_mappings(site)
+  table_items = []
+  country_code_mappings = {}
+  page = Nokogiri::HTML(open(site))
+
+  tds = page.css('table tr td')
+  td.each do |td|
+    #Nokogiri::XML::Element method
+    table_items << td.text
+  end
+
+  (0..table_items.length - 1).each do |index|
+    country_code_mappings[table_items[index + 1]] = table_items[index]
+  end
+
+  return country_code_mappings
+end
+
 #get a list of directories and parse the contents, writing to a compiled geocoordinate file.
 #TODO handle file errors
-def generate_resource_file(filename)
-  resource_file = open_file(filename, 'w+')
-  resource_file.puts "Latitude,Longitude,Location,Country Code"
-  
-  directories = Dir.glob('*').select {|f| File.directory? f}
+def generate_resource_file(output_filename, country_code_mappings)
+  resource_file = open_file(output_filename, 'w+')
+  resource_file.puts "Latitude,Longitude,Location,Country Code,Country Name"
+  geocoord_file = open_file(output_filename,"r")
+  File.open(resource_filepath, "r") do |geocoord_file|
+    directories = Dir.glob('*').select {|f| File.directory? f}
 
-  directories.each do |directory|
-    #remove the headers
-    system('tail -n +2 ' + directory + '/' + directory + '.txt' + ' > ' + directory + '/' + directory)
-    #write the new file
-    File.foreach(fileLocation) { |line| 
-      components = line.split "\t"
-      geocoordFile.puts components[3] + "," + components[4] + "," + components[22] + "," + directory.upcase
-    }
-    File.delete(directory + '/' + directory)
+    directories.each do |directory|
+      #remove the headers
+      system('tail -n +2 ' + directory + '/' + directory + '.txt' + ' > ' + directory + '/' + directory)
+      #write the new file
+      File.foreach(fileLocation) { |line| 
+        components = line.split "\t"
+        geocoord_file.puts components[3] + "," + components[4] + "," + components[22] + "," + directory.upcase, country_code_mappings[directory.upcase]
+      }
+      File.delete(directory + '/' + directory)
+    end
   end
 
   resource_file.close
 end
 
-#TODO: put in try to handle file errors
-def open_file(filepath, state)
-  return File.open(filepath, state)
-end
-
-def generate_json(resource_file, out_file)
-end
-
-def generate_sql(resource_file, out_file)
-end
-
-#TODO: check if resource_file exists
-def format_data(format, resource_filepath, out_filepath)
-   resource_file = open_file(resource_filepath, 'r')
-   out_file = open_file(out_filepath, 'w+')
-
-   case format
-   when "csv", "CSV"
-     if out_file != resource_file
-       `cp -a #{resource_file} #{out_file}`
-     end
-   when "json", "JSON"
-     generate_
-   when "sql", "SQL"
-
-   else
-     return "Format not available"
-   end
-
-   resource_file.close
-   out_file.close
-end
-
-
-class Parser
-  def self.parse(options)
-    args = {}
-
-    opt_parser = OptionParser.new do |opts|
-      opts.banner = "Usage: example.rb [options]"
-
-      opts.on("-r", "--retrieve", "Retrieve the geocoordinate resources") do |r|
-        args["retrieve"] = True
-      end
-
-      opts.on("-o", "--outputfilepath", "Filename for output") do |o|
-        args["outputfilepath"] = o
-      end
-
-      opts.on("-fFORMAT", "--formatFORMAT", "Save the geocoordinate resources in the available format") do |f|
-        args["format"] = f
-      end
-
-      opts.on("-h", "--help", "Prints this help") do
-        puts opts
-        exit
+def generate_json(resource_file)
+  #array for hashes of resource data to be converted to json
+  resources = []
+  
+  #loop through the resource
+  lines = IO.readlines(resource_file)
+  #Check file not empty
+  if lines.length > 0
+    resource = {}
+    headers = lines[0].chomp.split(",")
+    (1..lines.length() - 1).each do |line_index|
+      contents = lines[line_index].chomp.split(",")
+      if contents.length() == headers.length()
+        (0..headers.length() - 1).each do |index|
+          resource[headers[index]] = contents[index]
+        end
+        resources << resource
+      else
+        puts "Skipped due to mismatch of items to headers:" + lines[line_index]
       end
     end
+  else
+    abort("Resource file empty")
+  end
 
-    opt_parser.parse!(options)
-    return args
+  return JSON.pretty_generate(resources)
+end
+
+#TODO: generate code for multiple dbms using insert statements 
+def generate_sql(resource_file)
+end
+
+def create_data(format, resource_filepath, output_filepath)
+  if File.exist?(resource_filepath)
+    File.open(resource_filepath, "r") do |resource_file|
+      file_valid = false
+      begin
+        file_valid = resource_file.readline.chomp.eql?("Latitude,Longitude,Location,Country Code,Country Name")
+      rescue => e
+        abort(e.message)
+      end
+
+      if file_valid
+        File.open(output_filepath, "w+") do |output_file|
+          begin
+            case format
+            when "csv", "CSV"
+              if output_filepath != resource_filepath
+                `cp -a #{resource_filepath} #{output_filepath}`
+              end
+            when "json", "JSON"
+              output_file.write(generate_json(resource_file))
+            when "sql", "SQL"
+              output_file.write(generate_sql(resource_file))
+            else
+              abort("Format not available")
+            end
+          rescue => e
+            abort(e.message)
+          end
+        end
+      else
+        abort("Resource file not valid")
+      end
+    end
+  else
+    abort("Resource file not found")
   end
 end
 
-options = Parser.parse(ARGV)
-site = 'http://earth-info.nga.mil/gns/html/namefiles.html'
-resource_file = "geocoords"
 
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: geocoordinate_retriever_and_formatter.rb [options]"
+
+  opts.on("-d", "--download", "Force the download of the latest geocoordinate resources") do |d|
+    options[:download] = true
+  end
+
+  opts.on("-o", "--outputfilepath", "Filename for output") do |o|
+    options[:outputfilepath] = o
+  end
+
+  opts.on("-fFORMAT", "--formatFORMAT", "Save the geocoordinate resources in the available format") do |f|
+    options[:format] = f
+  end
+
+  opts.on("-rRESOURCE", "--resourceRESOURCE", "The location of the input geocoordinate file") do |f|
+    options[:resource] = f
+  end
+
+  opts.on("-h", "--help", "Prints this help") do
+    puts opts
+    exit
+  end
+
+end.parse!
+
+site = 'http://earth-info.nga.mil/gns/html/namefiles.html'
+
+resource_file = "geocoords"
+output_filepath = "output.csv"
+format = "CSV"
+
+if options.has_key?(:outputfilepath)
+  output_filepath = options[:outputfilepath]
+end
+
+if options.has_key?(:format)
+  format = options[:format]
+end
+
+if options.has_key?(:download)
+  retrieve_geocoordinate_resources(site)
+  generate_resource(resource_file, retrieve_country_code_mappings(site))
+end
+
+if options.has_key?(:resource)
+  resource_file = options[:resource]
+end
+
+create_data(format, resource_file, output_filepath)
